f7236812e141bf2249021e2bbe625d3a67b48bf6
Begu Maria Florina <maria.begu@info.uaic.ro>
2021-03-31 03:06:55 +0300

    Medical_App
    Login: Firebase
    Profile: Working on it

---
+++ LaB7/Try_01/app/src/main/java/com/example/try_01/AnomaliesActivity.java
@@ -0,0 +1,13 @@
+package com.example.try_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnomaliesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anomalies);
+    }
+}
