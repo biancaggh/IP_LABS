5fe4860e2f381165e3e2d6428dbe5bad7b84f109
stefiucmarina <79245738+stefiucmarina@users.noreply.github.com>
2021-04-13 04:45:18 +0300

    Cod pagina profil+meniu

---
+++ Pagina Profil + Meniu/java/MainActivity.java
@@ -0,0 +1,23 @@
+package com.profilepractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+ @Override
+    protected void onCreate(Bundle savedInstanceState){
+     super.onCreate(savedInstanceState);
+     setContentView(R.layout.activity_main);
+ }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.meniu, menu);
+        return true;
+    }
+}
