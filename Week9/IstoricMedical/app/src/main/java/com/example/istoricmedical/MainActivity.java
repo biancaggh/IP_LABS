3a1a703480d3480eba23788919e823677a504bdf
Begu Maria Florina <maria.begu@info.uaic.ro>
2021-04-13 22:52:15 +0300

    Week 9

---
+++ Week9/IstoricMedical/app/src/main/java/com/example/istoricmedical/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.istoricmedical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.meniu, menu);
+        return true;
+    }
+}
